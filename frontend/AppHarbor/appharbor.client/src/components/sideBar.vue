e3d7c752f0ef36e424b933e3722e151c73a0b893
PikaChu231121 <18250670351@163.com>
2024-07-09 15:46:25 +0800

    添加侧边栏组件

---
+++ frontend/AppHarbor/appharbor.client/src/components/sideBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div style="height: 100%">
+        <!-- title -->
+        <div class="list-title">{{ title }}</div>
+
+        <!-- list -->
+        <div class="list">
+            <div>
+                <div class="item__name">
+                </div>
+                <v-icon></v-icon>
+            </div>
+        </div>
+
+        <!-- no data -->
+        <div class="null-data"></div>
+    </div>
+</template>
