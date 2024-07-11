3395339b77668b7b09f7db6a98aa768b4a5dc58b
LAPTOP-0NEI987P\legion <qincheng037@gmail.com>
2024-07-11 12:07:47 +0800

    在第二天基础上实现后端按id查找好友

--- frontend/AppHarbor/AppHarbor.Server/Controllers/RelationshipController.cs
+++ frontend/AppHarbor/AppHarbor.Server/Controllers/RelationshipController.cs
@@ -24,6 +24,28 @@ namespace AppHarbor.Server.Controllers
             return Ok(_dbContext.Relationships.ToList());
         }
 
+
+        [HttpPost("findrelationship")]
+        public IActionResult FindRelationship([FromForm] decimal User1Id)
+        {
+            var query = from user in _dbContext.Relationships
+                        where user.User1Id == User1Id
+                        select new
+                        {
+                            yourFriend= user.User2Id
+                        };
+
+            var result = query.ToList();
+            //判断集合中是否有任何元素
+            //如果集合中有元素，返回 true；如果集合为空，返回 false。
+            if (!result.Any())
+            {
+                return NotFound("No matching records found");
+            }
+
+            return Ok(result);
+        }
+
     }
 }
 
