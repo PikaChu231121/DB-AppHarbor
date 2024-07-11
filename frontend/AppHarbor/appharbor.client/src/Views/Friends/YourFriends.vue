3c668b3a94962ed975e77d9c67690abe1e626281
Jackin Lin <1410524125@qq.com>
2024-07-11 16:09:27 +0800

    popup修复
    popup修复

---
+++ frontend/AppHarbor/appharbor.client/src/Views/Friends/YourFriends.vue
@@ -0,0 +1,10 @@
+{
+  // This file allows you to configure ESLint according to your project's needs, so that you
+  // can control the strictness of the linter, the plugins to use, and more.
+
+  // For more information about configuring ESLint, visit https://eslint.org/docs/user-guide/configuring/
+
+  "root": true,
+  "extends": "eslint:recommended",
+  "rules": {}
+}
