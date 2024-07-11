ef92cd0eb53e579c17b2784501fc6f6599f1cb3e
Jackin Lin <1410524125@qq.com>
2024-07-11 11:50:57 +0800

    登录动画啥的
    登录动画啥的，还有点小问题

--- frontend/AppHarbor/appharbor.client/src/Views/LoginAlert.vue
+++ frontend/AppHarbor/appharbor.client/src/Views/LoginAlert.vue
@@ -1,40 +1,53 @@
 <template>
-    <div v-if="visible" class="login-alert">
-        {{ message }}
+    <div v-if="visible" class="alert-box">
+        <div class="alert-content">
+            <p>{{ message }}</p>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
+        name: 'LoginAlert',
         props: ['message'],
         data() {
             return {
-                visible: false
+                visible: true
             };
         },
-        watch: {
-            message(newMessage) {
-                if (newMessage) {
-                    this.visible = true;
-                    setTimeout(() => {
-                        this.visible = false;
-                    }, 2000); // ÏûÏ¢ÏÔÊ¾2Ãë
-                }
+        mounted() {
+            setTimeout(() => {
+                this.closeAlert();
+            }, 2000); // Close the alert after 2 seconds
+        },
+        methods: {
+            closeAlert() {
+                this.visible = false;
+                this.$emit('close');
             }
         }
     };
 </script>
 
-<style>
-    .login-alert {
+<style scoped>
+    .alert-box {
         position: fixed;
-        bottom: 10px;
+        top: 0;
         left: 50%;
         transform: translateX(-50%);
-        background: #f8d7da;
-        color: #721c24;
-        padding: 10px 20px;
-        border: 1px solid #f5c6cb;
+        width: 100%;
+        max-width: 400px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        z-index: 1000;
+    }
+
+    .alert-content {
+        background: white;
+        padding: 20px;
         border-radius: 5px;
+        text-align: center;
+        margin-top: 20px; /* Add some margin to position it below the top edge */
     }
 </style>
