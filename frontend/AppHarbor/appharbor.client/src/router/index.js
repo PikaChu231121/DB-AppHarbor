a41675609c801b1dabe7ed42efc62b10ae284199
PikaChu231121 <18250670351@163.com>
2024-07-17 21:34:39 +0800

    合并前准备

--- frontend/AppHarbor/appharbor.client/src/router/index.js
+++ frontend/AppHarbor/appharbor.client/src/router/index.js
@@ -3,7 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
     {
         path: "/",
-        component: () => import("@/Views/Merchant/MerchantLogin.vue")
+        component: () => import("@/Views/UserLogin.vue")
+        // component: () => import("@/Views/Merchant/MerchantLogin.vue")
         // component: () => import("@/Views/Merchant/MainLayout.vue")
     },
     {
