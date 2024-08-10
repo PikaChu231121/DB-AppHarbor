36567cb2a21a6e4c426843d80eb43dc3034abb93
Jackin Lin <1410524125@qq.com>
2024-08-10 13:10:25 +0800

    管理员界面加了一些可爱的icon
    管理员界面加了一些可爱的icon

--- frontend/AppHarbor/appharbor.client/src/Views/AdminPage/Header.vue
+++ frontend/AppHarbor/appharbor.client/src/Views/AdminPage/Header.vue
@@ -3,7 +3,8 @@
         <div class="user-info">
             <img :src="adminAvatar" alt="Avatar" class="user-avatar" @click="togglePopup" />
             <div class="user-details">
-                <div class="user-id">管理员ID: {{ adminId }}</div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24"><path fill="currentColor" d="M12 14v2a6 6 0 0 0-6 6H4a8 8 0 0 1 8-8m0-1c-3.315 0-6-2.685-6-6s2.685-6 6-6s6 2.685 6 6s-2.685 6-6 6m0-2c2.21 0 4-1.79 4-4s-1.79-4-4-4s-4 1.79-4 4s1.79 4 4 4m9 6h1v5h-8v-5h1v-1a3 3 0 1 1 6 0zm-2 0v-1a1 1 0 1 0-2 0v1z" /></svg>
+                &nbsp;&nbsp;&nbsp;<div class="user-id">管理员ID: {{ adminId }}</div>
                 <div class="user-nickname">您好，{{ adminNickname }}管理员~</div>
             </div>
         </div>
--- frontend/AppHarbor/appharbor.client/src/Views/AdminPage/MainLayout.vue
+++ frontend/AppHarbor/appharbor.client/src/Views/AdminPage/MainLayout.vue
@@ -3,7 +3,8 @@
     <div class="main-layout">
         <div class="sidebar">
             <div @click="toggleSection('appManagement')" class="menu-item section-header">
-                应用管理
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10s-4.477 10-10 10M10.5 5.5l7 11m-4-11l-7 11m7-2.5h-7m11 0H16" /></svg>
+                &nbsp;&nbsp;&nbsp;应用管理
             </div>
             <div v-show="sections.appManagement" class="section-content">
                 <div class="menu">
@@ -17,7 +18,8 @@
             </div>
 
             <div @click="toggleSection('userManagement')" class="menu-item section-header">
-                用户管理
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M12.3 12.22A4.92 4.92 0 0 0 14 8.5a5 5 0 0 0-10 0a4.92 4.92 0 0 0 1.7 3.72A8 8 0 0 0 1 19.5a1 1 0 0 0 2 0a6 6 0 0 1 12 0a1 1 0 0 0 2 0a8 8 0 0 0-4.7-7.28M9 11.5a3 3 0 1 1 3-3a3 3 0 0 1-3 3m9.74.32A5 5 0 0 0 15 3.5a1 1 0 0 0 0 2a3 3 0 0 1 3 3a3 3 0 0 1-1.5 2.59a1 1 0 0 0-.5.84a1 1 0 0 0 .45.86l.39.26l.13.07a7 7 0 0 1 4 6.38a1 1 0 0 0 2 0a9 9 0 0 0-4.23-7.68" /></svg>
+                &nbsp;&nbsp;&nbsp;用户管理
             </div>
             <div v-show="sections.userManagement" class="section-content">
                 <div class="menu">
@@ -31,7 +33,8 @@
             </div>
 
             <div @click="toggleSection('merchant')" class="menu-item section-header">
-                商家管理
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M22 7.82a1.25 1.25 0 0 0 0-.19l-2-5A1 1 0 0 0 19 2H5a1 1 0 0 0-.93.63l-2 5a1.25 1.25 0 0 0 0 .19A.58.58 0 0 0 2 8a4 4 0 0 0 2 3.4V21a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-9.56A4 4 0 0 0 22 8a.58.58 0 0 0 0-.18M13 20h-2v-4h2Zm5 0h-3v-5a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v5H6v-8a4 4 0 0 0 3-1.38a4 4 0 0 0 6 0A4 4 0 0 0 18 12Zm0-10a2 2 0 0 1-2-2a1 1 0 0 0-2 0a2 2 0 0 1-4 0a1 1 0 0 0-2 0a2 2 0 0 1-4 .15L5.68 4h12.64L20 8.15A2 2 0 0 1 18 10" /></svg>
+                &nbsp;&nbsp;&nbsp;商家管理
             </div>
             <div v-show="sections.merchant" class="section-content">
                 <div class="menu">
@@ -45,14 +48,16 @@
             </div>
 
             <div @click="toggleSection('comment')" class="menu-item section-header">
-                评论管理
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="none"><path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" /><path fill="currentColor" d="M16 4a3 3 0 0 1 2.995 2.824L19 7v2a3 3 0 0 1 2.995 2.824L22 12v4a3 3 0 0 1-2.824 2.995L19 19v.966c0 1.02-1.143 1.594-1.954 1.033l-.096-.072L14.638 19H11a3 3 0 0 1-1.998-.762l-.14-.134L7 19.5c-.791.593-1.906.075-1.994-.879L5 18.5V17a3 3 0 0 1-2.995-2.824L2 14V7a3 3 0 0 1 2.824-2.995L5 4zm3 7h-8a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h3.638a2 2 0 0 1 1.28.464l1.088.906A1.5 1.5 0 0 1 18.5 17h.5a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1m-3-5H5a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h.5A1.5 1.5 0 0 1 7 16.5v.5l1.01-.757A3 3 0 0 1 8 16v-4a3 3 0 0 1 3-3h6V7a1 1 0 0 0-1-1" /></g></svg>
+                &nbsp;&nbsp;&nbsp;评论管理
             </div>
             <div v-show="sections.comment" class="section-content">
                 <!-- 评论相关内容 -->
             </div>
 
             <div @click="toggleSection('anouncement')" class="menu-item section-header">
-                通知管理
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20"><path fill="currentColor" d="M6 8v7h8V8a4 4 0 1 0-8 0m2.03-5.67a2 2 0 1 1 3.95 0A6 6 0 0 1 16 8v6l3 2v1H1v-1l3-2V8a6 6 0 0 1 4.03-5.67M12 18a2 2 0 1 1-4 0z" /></svg>
+                &nbsp;&nbsp;&nbsp;通知管理
             </div>
             <div v-show="sections.anouncement" class="section-content">
                 <!-- 通知相关内容 -->
